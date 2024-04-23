70f1e0e12c9125eb0a8a5a525aadb0b8021a4015
Juan Ramon Ruiz <159494661+JuanRamonRuizAguilar@users.noreply.github.com>
2024-04-23 14:42:25 -0600

    Add files via upload

---
+++ EVA3_1_ARREGLOS/src/main/java/com/mycompany/eva3_1_arreglos/EVA3_1_ARREGLOS.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eva3_1_arreglos;
+
+/**
+ *
+ * @author juani
+ */
+public class EVA3_1_ARREGLOS {
+
+    public static void main(String[] args) {
+        //LOS ARREGLOS DE JAVA SON OBJETOS 
+        //{} ---> mensaje los indicas arreglos acceso ala poscion 
+        int arreglo [] = new int [10];// un arreglo de 10 enteros
+        // a traves de indices 
+        // conocer las pociones de los valores 
+        // PRIMER POSOCION DE UN ARREGLO ES CERO --> O
+        // SEGUNDO POSICIONES DE UN ARREGLO ES ---> N - 1, DONDE 
+        // NES EL TAMAÑO DEL ARREGLO 
+        arreglo[1] = 100;
+        System.out.println("valor de arreglo[1] = " + arreglo[1]);
+        System.out.println(arreglo);
+        System.out.println(arreglo.length);// lentgth es la cantidad de elementos 
+        
+        
+        
+        
+    
+    
+
+    }
+}
