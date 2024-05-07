baf5958dededada3375624d0898a1f3816076080
Juan Ramon Ruiz <159494661+JuanRamonRuizAguilar@users.noreply.github.com>
2024-05-06 22:18:44 -0600

    Add files via upload

---
+++ EVA3_11_BUSQUEDA/src/eva3_11_busqueda/EVA3_11_BUSQUEDA.java
@@ -0,0 +1,43 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva3_11_busqueda;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author juani
+ */
+public class EVA3_11_BUSQUEDA {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+            // TODO code application logic here
+            //CREARR UN ARREGLO 
+            //LLENARLO CON VALORES ARRGLOS
+            //IMPRIMELO 
+            //ENCONTRAR AL NUMERO EL VALOR A BUSCAR
+                int arreglo[] = new int [10];
+                for(int i = 0; i < arreglo.length; i++){
+                    
+                }
+     Scanner captu = new Scanner(System.in);
+            System.out.println("que numero quiere buscar");
+                int valor = captu.nextInt();
+                //buscar
+                //recorrer el arreglo ciclo for
+                //comparar valor buscado contra el valor
+                //deberne buscador 
+             for (int i = 0; i < arreglo.length; i++){
+                 
+             }
+    }
+              
+           
+                
+       
+}
